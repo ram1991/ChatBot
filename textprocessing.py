5605d3c12bfaf3a9084f88fb2abc32fa0a649e97
Mouli Uddanti <uddanti.mouli91@gmail.com>
2018-10-08 14:11:28 +0530

    Update textprocessing.py

--- textprocessing.py
+++ textprocessing.py
@@ -28,4 +28,39 @@ for each_file in json_files:
                 input_texts.append(each_message[1])
             elif each_message[0] == 'participant2':
                 target_texts.append(each_message[1])
-                
+ 
+
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize 
+import string
+from string import punctuation
+from nltk.corpus import stopwords
+
+stop = set(stopwords.words('english'))
+stop_words = set(stopwords.words('english'))
+english_words = set(nltk.corpus.words.words())
+stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}']) # remove it if you need punctuation 
+
+
+def tokenize(text):
+    
+    tokenized_list = []
+
+    for each_sent in text: 
+       # print(each_sent)
+       temp_sent_list= []
+       sentences = sent_tokenize(each_sent)
+       temp_sent_list.append(sentences)
+       #print(sentences)
+       for each_word in sentences:
+           temp_word_list = []
+           if each_word not in stop_words and each_word not in string.punctuation:
+                if each_word in english_words or not each_word.isalpha():
+                    #print(each_word)
+                    words = word_tokenize(each_word.lower())
+                    temp_word_list.append(words)
+                    #words = re.sub('\d+','',str(words))
+                    #words = re.sub('[.,;?]','',str(words))
+                    tokenized_list.append(words)
+    return tokenized_list
+
