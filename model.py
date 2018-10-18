fe8d5e6915db33f30effd48e4bf1f70550e45aa8
Mouli Uddanti <uddanti.mouli91@gmail.com>
2018-10-18 19:29:36 +0530

    Update model.py

--- model.py
+++ model.py
@@ -11,6 +11,66 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.optimizers import SGD
 from importlib import reload
 
+num_words = 2000
+
+input_tokenizer = Tokenizer(num_words=num_words, lower=False, split=' ', filters='')
+input_tokenizer.fit_on_texts(tokenized_xtext)
+input_word_index = input_tokenizer.word_index
+target_tokenizer = Tokenizer(num_words=num_words, lower=False, split=' ', filters='')
+target_tokenizer.fit_on_texts(tokenized_ytext)
+target_word_index = target_tokenizer.word_index
+text_tokenizer = Tokenizer(num_words=num_words, lower=False, split=' ', filters='')
+text_tokenizer.fit_on_texts(tokenized_xtext + tokenized_ytext)
+word_index = text_tokenizer.word_index
+print(len(word_index))
+num_encoder_tokens =len(input_word_index)
+num_decoder_tokens = len(target_word_index)
+
+print(num_encoder_tokens)
+print(num_decoder_tokens)
+max_encoder_seq_length = max([len(txt) for txt in tokenized_xtext])
+max_decoder_seq_length = max([len(txt) for txt in tokenized_ytext])
+
+max_len = max(max_encoder_seq_length, max_decoder_seq_length)
+
+#print(len(tokenized_xtext))
+
+encoder_input_data = pad_sequences(input_tokenizer.texts_to_sequences(tokenized_xtext), maxlen=max_len)
+decoder_input_data = pad_sequences(target_tokenizer.texts_to_sequences(tokenized_ytext), maxlen=max_len)
+decoder_target_data = np.zeros(
+    (len(tokenized_ytext), max_len, num_decoder_tokens),
+    dtype='float32')
+
+
+for i, target_text in enumerate(tokenized_ytext):
+   # print(i,target_text)
+    for t, word in enumerate(target_text):
+        #print(i,word)
+#        w2idx = 0
+#        if word in target_word_index:
+#            w2idx = target_word_index[word]
+#        decoder_input_data[i, t, w2idx] = 1
+        if t>0:
+            decoder_target_data[i, t-1, target_word_index[word] -1] = 1
+  
+
+max_nb_words  = 2000
+embedding_dim = 300
+validation_split = 0.2
+vocab_size = len(word_index) + 1
+
+#encoder_input_data.shape
+
+nb_words = max(max_nb_words, len(word_index)) + 1
+
+embedding_matrix = np.zeros((nb_words, embedding_dim))
+
+for word, i in word_index.items():
+    print(word_index.items())
+    if word in word2Vecmodel.wv.vocab:
+        #print(word)
+        embedding_matrix[i] = word2Vecmodel[word]
+print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis = 1) == 0))
 
 embedding_layer = Embedding(input_dim = nb_words, output_dim = embedding_dim, trainable = True)
 embedding_layer.build((None,))
